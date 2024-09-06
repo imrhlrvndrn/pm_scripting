fea8c6093a455bab40f8ea6c838514d1ccbab017
Rahul Ravindran <43089715+imrhlrvndrn@users.noreply.github.com>
2024-09-06 15:39:46 +0530

    feat: added welcome & health check endpoints

--- index.js
+++ index.js
@@ -11,114 +11,120 @@ app.use(express.json());
 let db;
 
 (async () => {
-  db = await open({
-    filename: './database.sqlite',
-    driver: sqlite3.Database,
-  });
+    db = await open({
+        filename: './database.sqlite',
+        driver: sqlite3.Database,
+    });
 })();
 
-async function filterByDepartment(department) {
-  const query = 'SELECT * FROM employees WHERE department = ?';
-  const response = await db.all(query, [department]);
+app.get('/', (req, res) => {
+    res.send('Welcome to the Employees API');
+});
 
-  return { employees: response };
+app.get('/health', (req, res) => {
+    res.send('Employees API is operational!');
+});
+
+async function filterByDepartment(department) {
+    const query = 'SELECT * FROM employees WHERE department = ?';
+    const response = await db.all(query, [department]);
+
+    return { employees: response };
 }
 
 // /employees/department/Marketing
 app.get('/employees/department/:department', async (req, res) => {
-  const department = req.params.department;
+    const department = req.params.department;
 
-  try {
-    const results = await filterByDepartment(department);
+    try {
+        const results = await filterByDepartment(department);
 
-    if (results.employees.length === 0) {
-      return res
-        .status(404)
-        .json({ message: 'No employees found for department: ' + department });
+        if (results.employees.length === 0) {
+            return res
+                .status(404)
+                .json({ message: 'No employees found for department: ' + department });
+        }
+
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-
-    return res.status(200).json(results);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 });
 
 async function filterByJobTitle(job_title) {
-  const query = 'SELECT * FROM employees WHERE job_title = ?';
-  const response = await db.all(query, [job_title]);
+    const query = 'SELECT * FROM employees WHERE job_title = ?';
+    const response = await db.all(query, [job_title]);
 
-  return { employees: response };
+    return { employees: response };
 }
 
 // /employees/job_title/HR%20Manager
 app.get('/employees/job_title/:job_title', async (req, res) => {
-  const job_title = req.params.job_title;
+    const job_title = req.params.job_title;
 
-  try {
-    const results = await filterByJobTitle(job_title);
+    try {
+        const results = await filterByJobTitle(job_title);
 
-    if (results.employees.length === 0) {
-      return res
-        .status(404)
-        .json({ message: 'No employees found for job title: ' + job_title });
+        if (results.employees.length === 0) {
+            return res
+                .status(404)
+                .json({ message: 'No employees found for job title: ' + job_title });
+        }
+
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-
-    return res.status(200).json(results);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 });
 
 async function filterByLocation(location) {
-  const query = 'SELECT * FROM employees WHERE location = ?';
-  const response = await db.all(query, [location]);
+    const query = 'SELECT * FROM employees WHERE location = ?';
+    const response = await db.all(query, [location]);
 
-  return { employees: response };
+    return { employees: response };
 }
 
 // /employees/location/New%20York
 app.get('/employees/location/:location', async (req, res) => {
-  const location = req.params.location;
+    const location = req.params.location;
 
-  try {
-    const results = await filterByLocation(location);
+    try {
+        const results = await filterByLocation(location);
 
-    if (results.employees.length === 0) {
-      return res
-        .status(404)
-        .json({ message: 'No employees found for location: ' + location });
+        if (results.employees.length === 0) {
+            return res
+                .status(404)
+                .json({ message: 'No employees found for location: ' + location });
+        }
+
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-
-    return res.status(200).json(results);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 });
 
 async function filterByGender(gender) {
-  const query = 'SELECT * FROM employees WHERE gender = ?';
-  const response = await db.all(query, [gender]);
+    const query = 'SELECT * FROM employees WHERE gender = ?';
+    const response = await db.all(query, [gender]);
 
-  return { employees: response };
+    return { employees: response };
 }
 
 // /employees/gender/male
 app.get('/employees/gender/:gender', async (req, res) => {
-  const gender = req.params.gender;
+    const gender = req.params.gender;
 
-  try {
-    const results = await filterByGender(gender);
+    try {
+        const results = await filterByGender(gender);
 
-    if (results.employees.length === 0) {
-      return res
-        .status(404)
-        .json({ message: 'No employees found for gender: ' + gender });
+        if (results.employees.length === 0) {
+            return res.status(404).json({ message: 'No employees found for gender: ' + gender });
+        }
+
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-
-    return res.status(200).json(results);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 });
 
 app.listen(PORT, () => console.log('Server running on port 3000'));
